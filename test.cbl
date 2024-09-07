413d45d34aa66faec69224858c00464567c2a31f
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-09-07 09:17:07 -0300

    Buenas practicas en cobol
    Buenas practicas en cobol

--- test.cbl
+++ test.cbl
@@ -1,5 +1,5 @@
        IDENTIFICATION DIVISION.
-       PROGRAM-ID. CAPITULO-7.
+       PROGRAM-ID. CAPITULO-8.
        ENVIRONMENT DIVISION.
        CONFIGURATION SECTION.
            SPECIAL-NAMES.
@@ -7,6 +7,11 @@
                SYMBOLIC CHARACTERS beta IS 226
                                    niebla IS 177
                                    nieve IS 178.
+       INPUT-OUTPUT SECTION.
+           FILE-CONTROL.
+               SELECT OPTIONAL archivo-empleados
+               ASSIGN TO "emplados.dat"
+               ORGANIZATION IS SEQUENTIAL.
 
        DATA DIVISION.
        FILE SECTION.
@@ -16,4 +21,4 @@
             DISPLAY nieve nieve niebla niebla beta beta beta beta niebla
             niebla nieve nieve.
             STOP RUN.
-       END PROGRAM CAPITULO-7.
+       END PROGRAM CAPITULO-8.
