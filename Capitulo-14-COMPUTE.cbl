701fbf7d8c70473a1482b43e5b0188063a8ee6b9
Matias Daniel Alessandrello <135188526+malessan4@users.noreply.github.com>
2024-09-08 19:18:46 -0300

    Probamos la accion COMPUTE
    Con diferentes formas de aplicarlo. Suma, resta, multiplicación, división.

--- Capitulo-14-COMPUTE.cbl
+++ Capitulo-14-COMPUTE.cbl
@@ -4,11 +4,22 @@
        DATA DIVISION.
        FILE SECTION.
        WORKING-STORAGE SECTION.
+      *>   con ctrl + / se comenta la linea seleccionada
       *>   COMPUTE sirve para evaluar expresiones aritmeticas
       *>   Se puede hacer 4 operaciones aritmeticas
            01 Numero1 PIC 99 VALUE 25.
            01 Numero2 PIC 99 VALUE 15.
+           01 num1 PIC 99 VALUE 4.
+           01 num2 PIC 99 VALUE 2.
+
+
            01 ResultadoSuma1 PIC 99 VALUE 0.
+           01 ResultadoResta1 PIC 99 VALUE 0.
+           01 ResultadoMulti1 PIC 99 VALUE 0.
+           01 ResultadoDivision1 PIC 9(4)V9(4) VALUE 0.
+      *>   EL V9 es para la coma decimal
+           01 ResultadoCuenta1 PIC 99999 VALUE 0.
+
 
        PROCEDURE DIVISION.
            CalculayMuestra.
@@ -16,5 +27,27 @@
             " es: ".
             COMPUTE ResultadoSuma1 = Numero1 + Numero2.
             DISPLAY ResultadoSuma1.
+
+            DISPLAY "El resultado de la resta de " Numero1 " y "Numero2
+            " es: ".
+            COMPUTE ResultadoResta1 = Numero1 - Numero2.
+            DISPLAY ResultadoResta1.
+
+            DISPLAY "El resultado de la multiplicacion de " num1 " y "
+             num2 " es: ".
+            COMPUTE ResultadoMulti1 = num1 * num2.
+            DISPLAY ResultadoMulti1.
+      *>   En las multiplicaciones para calcular el picture se pone el
+      *>   doble que de los operandos
+
+            DISPLAY "El resultado de la division de " Numero1 " y "
+             Numero2 " es: ".
+            COMPUTE ResultadoDivision1 = Numero1 / Numero2.
+            DISPLAY ResultadoDivision1.
+
+      *>   Los signos de operaciones deben ir separados de los numeros
+            COMPUTE ResultadoCuenta1 = 20 * 5 + 25 + (22 - 6).
+            DISPLAY "El resultado de: 20 * 5 + 25 + (22-6) es igual = "
+            ResultadoCuenta1
             STOP RUN.
        END PROGRAM Capitulo14.
